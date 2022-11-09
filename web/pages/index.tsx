c2d5bf46b034ddeae7e30ec2f212a2ce4e3cae36
Karlisson <karlissonferreira42@gmail.com>
2022-11-09 11:38:23 -0300

    error solving

---
+++ web/pages/index.tsx
@@ -0,0 +1,17 @@
+
+export default function Home() {
+  return (
+    <h1>Hello world</h1>
+  )
+}
+
+export const getServerSideProps = async() => {
+  const response = await fetch('http://localhost:3333/pools/count')
+  const data = await response.json()
+  
+    return{
+      props:{
+        count: data.count,
+      }
+    }  
+}
